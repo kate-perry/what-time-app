684bab83be5befadc3913f0dfb216bc9b03ce8e2
Kate Perry <thekateperry@gmail.com>
2022-07-30 00:47:17 -0400

    light setup with styling, window size, dark mode handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, createContext } from 'react';
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { getWindowSize } from './shared/utils/functions';
+import { getDesignTokens } from './shared/styles/MuiThemes'
+import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles';
+import './shared/styles/styles.scss';
+
+export const WindowSizeContext = createContext(getWindowSize());
 
 function App() {
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize())
+    }
+    window.addEventListener('resize', handleWindowResize);
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = React.useMemo(() => responsiveFontSizes(createTheme(getDesignTokens(prefersDarkMode ? 'dark' : 'light'))), [prefersDarkMode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WindowSizeContext.Provider value={windowSize} >
+      <ThemeProvider theme={theme}>
+        <div className="app">
+          <Box
+            sx={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              bgcolor: 'background.default',
+              color: 'text.primary',
+              p: 3,
+            }}
+          >
+            <Typography variant="body1">This is a {theme.palette.mode} mode theme with custom palette</Typography>
+          </Box>
+        </div>
+      </ThemeProvider>
+    </WindowSizeContext.Provider>
   );
 }
 
